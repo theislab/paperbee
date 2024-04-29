1f44f3d1aebc195729eed6f943c6f5f44211f652
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2024-04-29 17:53:19 +0200

    Escape restricted symbols

--- papers/telegram_papers_formatter.py
+++ papers/telegram_papers_formatter.py
@@ -3,6 +3,12 @@ from typing import List, Tuple
 from telegram import Bot
 
 
+def escape_reserved_symbols(text: str, symbols="!.-+>()") -> str:
+    for symbol in symbols:
+        text = text.replace(symbol, f"\\{symbol}")
+    return text
+
+
 class TelegramPaperPublisher:
     def __init__(
         self, logger: Logger, channel_id: str = None, bot_token: str = None
@@ -19,7 +25,10 @@ class TelegramPaperPublisher:
         preprints = []
         for paper in papers_list:
             emoji = "✏️" if paper[3] == "TRUE" else "🗞️"
-            formatted_paper = f"{emoji} <{paper[-1]}|{paper[4]}>"
+            title = escape_reserved_symbols(paper[4])
+            link = escape_reserved_symbols(paper[-1])
+            formatted_paper = f"{emoji} [{title}]({link})"
+            
             if paper[3] == "TRUE":
                 preprints.append(formatted_paper)
             else:
@@ -34,11 +43,11 @@ class TelegramPaperPublisher:
         today: str,
         spreadsheet_id: str,
     ) -> None:
-        divider = "────────────"
+        divider = escape_reserved_symbols("────────────")
 
         try:
-            header = "Good morning ☕ Here are today's papers! Enjoy your reading! 👋\n"
-            footer = f"*View all papers:* <https://docs.google.com/spreadsheets/d/{spreadsheet_id}|Google Sheet> :books:"
+            header = "Good morning ☕ Here are today's papers\\! Enjoy your reading\\! 👋\n"
+            # footer = f"*View all papers:* [Google Sheet](https://docs\\.google\\.com/spreadsheets/d/{spreadsheet_id}) :books:"
             message_blocks = [
                 header,
                 "*Preprints:*👇"
@@ -47,7 +56,7 @@ class TelegramPaperPublisher:
                 for paper in preprints:
                     message_blocks.append(paper)
             else:
-                message_blocks.append("No preprints found today.")
+                message_blocks.append("No preprints found today\\.")
 
             message_blocks.append(divider)
             message_blocks.append("*Papers:*👇")
@@ -56,16 +65,19 @@ class TelegramPaperPublisher:
                 for paper in papers:
                     message_blocks.append(paper)
             else:
-                message_blocks.append("No papers found today.")
+                message_blocks.append("No papers found today\\.")
 
             message_blocks.append(divider)
-            message_blocks.append(footer)
-            message_blocks.append(f"Published on {today}")
-            message_blocks.append("Posted with `slack-papers-app` <https://github.com/lueckenlab/slack_papers_bot|GitHub>")
+            # message_blocks.append(footer)
+            message_blocks.append(f"Published on {escape_reserved_symbols(today)}")
+            message_blocks.append("Posted with `slack\\-papers\\-app` [GitHub](https://github\\.com/lueckenlab/slack_papers_bot)")
 
-            response = self.bot.sendMessage(
+            print("MESSAGE:", "\n".join(message_blocks))
+
+            response = self.bot.send_message(
                 chat_id=self.channel_id,
-                text=header + "".join(message_blocks),
+                text="\n".join(message_blocks),
+                parse_mode="MarkdownV2"
             )
 
             self.logger.info(f"Published papers to Telegram: {response}")
