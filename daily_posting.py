032ce0f802fd425814b1b700b37d173e0b8a16f5
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2024-04-29 17:58:12 +0200

    Post message to telegram

--- daily_posting.py
+++ daily_posting.py
@@ -9,5 +9,9 @@ finder = PapersFinder(
     sheet_name="Papers",
     llm_filtering=True,
     interactive=False,
+    slack_bot_token=config.SLACK_BOT_TOKEN,
+    slack_channel_id=config.SLACK_CHANNEL_ID,
+    telegram_bot_token=config.SLACK_BOT_TOKEN,
+    telegram_channel_id=config.SLACK_BOT_TOKEN,
 )
-papers, response = finder.run_daily()
+papers, response = finder.run_daily(post_to_slack=True, post_to_telegram=True)
