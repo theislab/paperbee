d8f6f180ad7ca1cbefe95f7c34d50a8f0142ca82
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-05-14 15:52:58 +0200

    Update imports

--- src/PapersBee/papers/__init__.py
+++ src/PapersBee/papers/__init__.py
@@ -1,5 +1,5 @@
 from . import config
 from .papers_finder import PapersFinder
-from .validate_inputs import validate_configuration, validate_llm_args, validate_ncbi_api_key, validate_posting_args
+from .validate_inputs import validate_configuration, validate_llm_args, validate_platform_args
 
-__all__ = ['PapersFinder', 'config', 'validate_configuration', 'validate_llm_args', 'validate_ncbi_api_key', 'validate_posting_args']
+__all__ = ['PapersFinder', 'config', 'validate_configuration', 'validate_llm_args', 'validate_platform_args']
