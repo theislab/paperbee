455320e52064c796af48cf2731468bffb7dbf9a7
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-05-14 15:20:55 +0200

    Remove unnecessary file, as it duplicates config

--- src/PapersBee/papers/posting_required_params.py
+++
@@ -1,6 +0,0 @@
-# Mapping of social platforms to their required config parameter names
-SOCIAL_REQUIRED_PARAMS = {
-    'slack': ['SLACK_BOT_TOKEN', 'SLACK_CHANNEL_ID', 'SLACK_APP_TOKEN'],
-    'zulip': ['ZULIP_PRC', 'ZULIP_STREAM', 'ZULIP_TOPIC'],
-    'telegram': ['TELEGRAM_BOT_API_KEY', 'TELEGRAM_CHANNEL_ID'],
-}
